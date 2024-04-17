bb917d4cb6568a53edc28dbe6e0a78b24927590f
BigStar2024 <138141789+BigStar-2024@users.noreply.github.com>
2024-04-18 05:15:40 +1000

    Violation project start

---
+++ src/pages/First.tsx
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+
+const First = () => {    
+    return (
+      <>
+      <div>
+        afsfew
+      </div>
+      </>
+    );
+  };
+  
+  export default First;
+;
+  
